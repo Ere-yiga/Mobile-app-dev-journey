a3756bc3d3a071ff4910a74082a41ce03dbcde02
Ere-yiga <erehdagr8@gmail.com>
2025-09-28 16:18:45 +0100

    a simple quiz app to practice stdin and stdout

--- quiz/quiz.dart
+++ quiz/quiz.dart
@@ -18,31 +18,3 @@ void main(){
 
   print('You have a score of $score/2');
 }
-
-
-
-
-
-/* import 'dart:io';
-
-void main() {
-  while (true) {
-    var score = 0;
-    stdout.write(
-        'What is dart primarily used for? \nA)Graphic design \nB) Mobile and web dev ');
-    var one = stdin.readLineSync();
-
-    stdout.write(
-        'Which operator is used for equality comparison? \nA)== \nB)=== \nC)= \nD)!= ');
-    var two = stdin.readLineSync();
-
-    if (one == 'B') {
-      score++;
-    }
-    if (two == 'B') {
-      score++;
-    }
-    print('You scoreed $score/2');
-  }
-}
- */
