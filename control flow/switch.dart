84eb1c7ad3d9b6d9eb995b9073cafc2d537fefd8
Ere-yiga <erehdagr8@gmail.com>
2025-10-02 05:21:35 +0100

    learnt more about control flow

---
+++ control flow/switch.dart
@@ -0,0 +1,13 @@
+void main(){
+  var day = 3;
+  switch(day){
+    case 1:
+    print('Today is Monday');
+    break;
+    case 2:
+    print('Today is Tuesday');
+    break;
+    default:
+    print('Enter a valid number');
+  }
+}
---
+++ loops/loop.dart
@@ -0,0 +1,9 @@
+void main(){
+  for(var i = 10; i >= 0;i--){
+    print(i);
+  }
+  var num = [1, 2, 3];
+  for(var nums in num){
+    print(nums);
+  }
+}
