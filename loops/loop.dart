04d249c11d4d542226f151aaf8f66e0b913d1333
Ere-yiga <erehdagr8@gmail.com>
2025-10-02 06:04:14 +0100

    lerant more about control flow

--- loops/loop.dart
+++ loops/loop.dart
@@ -1,9 +1,27 @@
 void main(){
+  //for
   for(var i = 10; i >= 0;i--){
     print(i);
   }
+
+  //for...in
   var num = [1, 2, 3];
   for(var nums in num){
     print(nums);
   }
+
+//while
+  var i = 2;
+  while(i <= 10){
+    print(i);
+    i++;
+  }
+
+//do...while
+  var a = 5;
+  var maxnum = 10;
+  do{
+    print(a);
+    a += 1;
+  }while(a < maxnum);
 }
