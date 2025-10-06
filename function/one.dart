b0d3a7f4d89123fd6d4f266a2cdc17e68d74b102
Ere-yiga <erehdagr8@gmail.com>
2025-10-06 17:43:02 +0100

    anonymous function

--- function/one.dart
+++ function/one.dart
@@ -1,9 +1,16 @@
 void main(){
   myName();
+
+  
+  var fruits = ['Apple', 'Banana', 'Cherry'];
+
+  fruits.forEach((fruit){
+    print(fruit);
+  });
 }
 
 
 //Anonymous function
 var myName = (){
-  print('Hello, I am Ereh and it s nice meeting you');
+  print("Hello, I am Ereh. It's ice meeting you");
 };
