d16a8445956fdf75d601b8707cef77922deede48
Ere-yiga <erehdagr8@gmail.com>
2025-10-07 08:55:34 +0100

    continuation on function

--- function/one.dart
+++ function/one.dart
@@ -1,12 +1,23 @@
 void main(){
   myName();
 
-  
+  //Example 1
   var fruits = ['Apple', 'Banana', 'Cherry'];
 
   fruits.forEach((fruit){
     print(fruit);
   });
+
+  //Example 2
+  var items = ['shoes', 'cloth', 'cap'];
+
+  items.forEach((item) {
+    print(item);
+  });
+
+  //Still arrow functionn
+  printName();
+
 }
 
 
@@ -14,3 +25,6 @@ void main(){
 var myName = (){
   print("Hello, I am Ereh. It's ice meeting you");
 };
+
+//Arrow function
+void printName() => print('My name is Ereyiga');
---
+++ function/return.dart
@@ -0,0 +1,8 @@
+void main(){
+  final me = describe('Jonah');
+  print(me);
+}
+
+  describe(name){
+  return "I am $name and it's a pleasure meeting you.";
+}
